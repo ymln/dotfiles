459df812a182f4bc0ef263bae6fa8dc5ee67de5c
Yuriy Melnyk <ymelnik@yandex.ru>
2015-11-08 12:40:29 +0200

    wip

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 git init --separate-git-dir=$HOME/.githome
 git remote add origin git@github.com:ymln/dotfiles.git
 git fetch
-gh checkout -t origin/master
+git checkout -t origin/master
 rm .git
 bash -c "`curl -sL get.freshshell.com`"
-mkdir -p ~/.vim/bundle/
-pushd ~/.vim/bundle/
-git clone https://github.com/VundleVim/Vundle.vim.git vundle
-popd
+mkdir -p .vim/bundle/
 mkdir -p .vim/{backup,swap}
