1e4166b6f50ce97e477513f632c0f1db24aa8da4
Yuriy Melnyk <ymelnik@yandex.ru>
2016-02-11 14:35:41 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
-# To run: curl https://raw.githubusercontent.com/ymln/dotfiles/master/install.sh | bash
+# Install zsh, git, base-devel/build-essential
+# To run: curl https://raw.githubusercontent.com/ymln/dotfiles/master/install.sh | zsh
 git init --separate-git-dir=$HOME/.githome
 git remote add origin git@github.com:ymln/dotfiles.git
 git fetch
 git checkout -t origin/master
 rm .git
 
-bash -c "`curl https://raw.githubusercontent.com/freshshell/fresh/master/install.sh`"
+zsh -c "`curl https://raw.githubusercontent.com/freshshell/fresh/master/install.sh`"
 source .fresh/build/shell.sh
 fresh
 
