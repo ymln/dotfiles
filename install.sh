489352d5ebeafd43b873c3b713ac2069372e1524
Yuriy Melnyk <ymelnik@yandex.ru>
2016-02-11 14:54:09 +0200

    wip

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ git checkout -t origin/master
 rm .git
 
 git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh
+# TODO
+pushd ~/.fresh/source/freshshell/fresh && git checkout pre && popd
 ~/.fresh/source/freshshell/fresh/bin/fresh
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
