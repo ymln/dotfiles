8566bc91f9d1fb7b61508ad19ba3b9927e9604a4
Yuriy Melnyk <ymelnik@yandex.ru>
2016-02-11 14:33:33 +0200

    wip

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# To run: bash -c `curl https://raw.githubusercontent.com/ymln/dotfiles/master/install.sh`
+# To run: curl https://raw.githubusercontent.com/ymln/dotfiles/master/install.sh | bash
 git init --separate-git-dir=$HOME/.githome
 git remote add origin git@github.com:ymln/dotfiles.git
 git fetch
@@ -7,6 +7,7 @@ git checkout -t origin/master
 rm .git
 
 bash -c "`curl https://raw.githubusercontent.com/freshshell/fresh/master/install.sh`"
+source .fresh/build/shell.sh
 fresh
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
